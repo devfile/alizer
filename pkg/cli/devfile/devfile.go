2ad7582f662793a03f1b1ef86d4a5f44f362c5ca
Michael Hoang <35011707+mike-hoang@users.noreply.github.com>
2023-06-30 08:49:17 -0400

    flattening go dir (#1)
    Signed-off-by: Michael Hoang <mhoang@redhat.com>
    Signed-off-by: thepetk <thepetk@gmail.com>

---
+++ pkg/cli/devfile/devfile.go
@@ -0,0 +1,38 @@
+package devfile
+
+import (
+	"github.com/devfile/alizer/pkg/apis/recognizer"
+	"github.com/devfile/alizer/pkg/utils"
+	"github.com/spf13/cobra"
+)
+
+var logLevel, registry string
+
+func NewCmdDevfile() *cobra.Command {
+	devfileCmd := &cobra.Command{
+		Use:   "devfile",
+		Short: "Select a devfile from a list of existing devfiles (from a devfile registry or other storage) based on the information extracted from the source tree.",
+		Long:  "Select a devfile from a list of existing devfiles (from a devfile registry or other storage) based on the information extracted from the source tree.",
+		Args:  cobra.MaximumNArgs(1),
+		Run:   doSelectDevfile,
+	}
+	devfileCmd.Flags().StringVar(&logLevel, "log", "", "log level for alizer. Default value: error. Accepted values: [debug, info, warning]")
+	devfileCmd.Flags().StringVarP(&registry, "registry", "r", "", "registry where to download the devfiles. Default value: https://registry.devfile.io")
+	return devfileCmd
+}
+
+func doSelectDevfile(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		utils.PrintNoArgsWarningMessage(cmd.Name())
+		return
+	}
+	if registry == "" {
+		registry = "https://registry.devfile.io/index"
+	}
+	err := utils.GenLogger(logLevel)
+	if err != nil {
+		utils.PrintWrongLoggingLevelMessage(cmd.Name())
+		return
+	}
+	utils.PrintPrettifyOutput(recognizer.SelectDevFilesFromRegistry(args[0], registry))
+}
