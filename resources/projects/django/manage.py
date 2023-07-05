67721892e1d4b6354d315ec214d30a4b840c2067
thepetk <thepetk@gmail.com>
2023-07-05 12:49:28 +0100

    Update django test resources
    Signed-off-by: thepetk <thepetk@gmail.com>

--- resources/projects/django/manage.py
+++ resources/projects/django/manage.py
@@ -8,3 +8,5 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+    from django.core.management.commands.runserver import Command as runserver
+    runserver.default_port = "3543"
