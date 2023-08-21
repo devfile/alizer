53325785033b8e99e9d366980fcd1b18f956d88c
thepetk <thepetk@gmail.com>
2023-08-21 16:04:40 +0100

    Add tests for detectComponentsWithSettings
    Signed-off-by: thepetk <thepetk@gmail.com>

--- pkg/apis/recognizer/component_recognizer_test.go
+++ pkg/apis/recognizer/component_recognizer_test.go
@@ -2,7 +2,10 @@ package recognizer
 
 import (
 	"context"
+	"os"
+	"path/filepath"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/devfile/alizer/pkg/apis/model"
@@ -100,4 +103,77 @@ func Test_isAnyComponentInDirectPath(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_detectComponentsWithSettings(t *testing.T) {
+	tests := []struct {
+		name               string
+		path               string
+		expectedComponents []model.Component
+		expectingError     bool
+	}{
+		{
+			name: "Case 1: detect components",
+			path: "../../../resources/projects/beego",
+			expectedComponents: []model.Component{
+				{
+					Name: "beego",
+					Path: "../../../resources/projects/beego",
+				},
+			},
+			expectingError: false,
+		},
+		{
+			name:               "Case 2: invalid path",
+			path:               "../../../resources/projects/notexisting",
+			expectedComponents: []model.Component{},
+			expectingError:     true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			detectionSettings, err := getDetectionSettings(tt.path)
+			if err != nil {
+				t.Errorf("test failed: Couldn't locate current working directory")
+			}
+			ctx := context.Background()
+			result, err := detectComponentsWithSettings(detectionSettings, &ctx)
+			if tt.expectingError {
+				if err == nil {
+					t.Errorf("test Failed: Was expecting path not found")
+				}
+				assert.EqualValues(t, 0, len(result))
+			} else {
+				if len(result) != 1 {
+					t.Errorf("expected 1 component for %s dir", tt.path)
+				}
+				expectedPath, err := getAbsolutePath(tt.expectedComponents[0].Path)
+				if err != nil {
+					t.Errorf("test failed: %s", err)
+				}
+				assert.EqualValues(t, tt.expectedComponents[0].Name, result[0].Name)
+				assert.EqualValues(t, expectedPath, result[0].Path)
+			}
+		})
+	}
+}
+
+func getDetectionSettings(path string) (model.DetectionSettings, error) {
+	absPath, err := getAbsolutePath(path)
+	if err != nil {
+		return model.DetectionSettings{}, err
+	}
+	return model.DetectionSettings{
+		BasePath:              absPath,
+		PortDetectionStrategy: []model.PortDetectionAlgorithm{model.Compose, model.DockerFile, model.Source},
+	}, nil
+}
+
+func getAbsolutePath(path string) (string, error) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		return "", err
+	}
+	return strings.Join([]string{filepath.Clean(filepath.Join(pwd, path)), "/"}, ""), nil
 }
