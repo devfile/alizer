fd746b2e6edf4d571fe755c612cfb6ef2b8b0580
thepetk <thepetk@gmail.com>
2023-08-22 11:42:59 +0100

    Revert global var detectComponentsWithPathAndPortStartegy
    Signed-off-by: thepetk <thepetk@gmail.com>

--- pkg/apis/recognizer/component_recognizer.go
+++ pkg/apis/recognizer/component_recognizer.go
@@ -60,8 +60,7 @@ func DetectComponentsWithPathAndPortStartegy(path string, portDetectionStrategy 
 	return detectComponentsWithPathAndPortStartegy(path, portDetectionStrategy, &ctx)
 }
 
-// detectComponentsWithPathAndPortStartegy is exposed as a global variable for the purpose of running mock tests
-var detectComponentsWithPathAndPortStartegy = func(path string, portDetectionStrategy []model.PortDetectionAlgorithm, ctx *context.Context) ([]model.Component, error) {
+func detectComponentsWithPathAndPortStartegy(path string, portDetectionStrategy []model.PortDetectionAlgorithm, ctx *context.Context) ([]model.Component, error) {
 	return detectComponentsWithSettings(model.DetectionSettings{
 		BasePath:              path,
 		PortDetectionStrategy: portDetectionStrategy,
