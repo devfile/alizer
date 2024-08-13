20ed31ebcb2a1716d3728a2ed7ba0cf938f9149a
thepetk <thepetk@gmail.com>
2024-08-13 15:30:16 +0100

    Restructure test resource
    Signed-off-by: thepetk <thepetk@gmail.com>

--- test/apis/language_recognizer_test.go
+++ test/apis/language_recognizer_test.go
@@ -50,8 +50,8 @@ func TestAnalyzeOnGoGin(t *testing.T) {
 	isLanguageInProject(t, "golang-gin-app", "go", []string{"1.15"}, []string{"gin"})
 }
 
-func TestAnalyzeCoolStore(t *testing.T) {
-	isLanguageInProject(t, "coolstore", "java", []string{}, []string{})
+func TestAnalyzeJSStaticFilesInJavaApp(t *testing.T) {
+	isLanguageInProject(t, "js-static-files-in-java-app", "java", []string{}, []string{})
 }
 
 func isLanguageInProject(t *testing.T, project string, wantedLanguage string, wantedTools []string, wantedFrameworks []string) {
